e14fe910658f6e63bf71d02db48a3b9328067ee4
Yesha612 <yesha06121999@gmail.com>
2019-06-19 00:56:05 -0400

    Added gitignore and views for all pages

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/aboutme', (req, res, next) => {
+  res.render('aboutme', {});
+});
+
+router.get('/projects', (req, res, next) => {
+  res.render('projects', {});
+});
+
+router.get('/services', (req, res, next) => {
+  res.render('services', {});
+});
+
+router.get('/contactme', (req, res, next) => {
+  res.render('contactme', {});
+});
+
+module.exports = router;
